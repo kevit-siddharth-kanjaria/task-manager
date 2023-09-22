7ae9e7ca7032451828d71d5268185827a23608f2
Siddharth Kanjaria <siddharth.kanjaria@kevit.io>
2023-09-22 15:15:18 +0530

    create user and task implemented

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
 const express = require('express')
+require('./db/mongoose')
+const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
-
 const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.post('/users', async (req,res)=>{
+    
+    const user = new User(req.body)
+
+    try {
+        const newUser = await user.save()
+        res.send(newUser);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+})
+
+
+app.post('/tasks', async (req,res)=>{
+    
+    const task = new Task(req.body)
+
+    try {
+        const newTask = await task.save()
+        res.send(newTask);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+})
 
 app.listen(port,()=>{
     console.log("server is active on port "+port);
